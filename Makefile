2e5f7d734781673613d35de3d615e7938e50e180
Jaon <jamonsterr@gmail.com>
2017-09-06 16:58:51 -0300

    Makefile

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 CC = $(shell which gcc)
 SOURCES = $(wildcard src/*)
 OBJECTS = $(SOURCES:%.c=%.o)
+INCLUDES = -Iinclude -I/home/jaon/Projects/v8/include
 
 %.o: %.c
-	$(CC) -c -g $< -o $@
+	$(CC) -c -g $(INCLUDES) $< -o $@
 
 program: $(OBJECTS)
-	$(CC) -g -o $@ $^ -lglfw #-Lhome/jaon/Projects/v8/out/x86.release -l v8
+	$(CC) -g -o $@ $^ -lglfw -lGL -L/home/jaon/Projects/v8/out/x64.release -l v8
 
 clean:
 	rm -rf $(OBJECTS) program
--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <v8.h>
 
 int main(int argc, char* argv[]) {
+  if (!glfwInit()) {
+
+  }
+
+  GLFWwindow *window = glfwCreateWindow(500, 400, "plutaria", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(1, 1, 1, 1);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   return 0;
 }
